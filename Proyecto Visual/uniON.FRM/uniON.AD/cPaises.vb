d466c1f78cdc2b27b64b5dc27dc2bb23fa1670a3
SantiFessia12 <56371081+SantiFessia12@users.noreply.github.com>
2020-04-28 15:28:44 -0300

    commit.Santi
    Form
    frmPais/cPais
    frmProvincia/cProvincia

---
+++ Proyecto Visual/uniON.FRM/uniON.AD/cPaises.vb
@@ -0,0 +1,48 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports Microsoft.Practices.EnterpriseLibrary.Data
+Public Class cPaises
+    Dim oDatabase As Database
+
+    Public Sub New()
+        oDatabase = DatabaseFactory.CreateDatabase("Conn")
+    End Sub
+
+    Public Sub New(ByVal str As String)
+    End Sub
+
+    Public Function BuscarPorId(ByVal IdRubro As Integer) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("PaisBuscarPorId", IdRubro)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function BuscarTodos() As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("PaisBuscarTodos")
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function Agregar(ByVal Pais As String, ByVal Activo As Boolean) As Double
+        Try
+            Return oDatabase.ExecuteScalar("PaisAgregar", Pais, Activo)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function Modificar(ByVal IdPais As Integer, ByVal Pais As String, ByVal Activo As Boolean) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("PaisModificar", IdPais, Pais, Activo)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+End Class
---
+++ Proyecto Visual/uniON.FRM/uniON.AD/cProvincias.vb
@@ -0,0 +1,56 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports Microsoft.Practices.EnterpriseLibrary.Data
+Public Class cProvincias
+    Dim oDatabase As Database
+
+    Public Sub New()
+        oDatabase = DatabaseFactory.CreateDatabase("Conn")
+    End Sub
+
+    Public Sub New(ByVal str As String)
+    End Sub
+
+    Public Function BuscarTodos() As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("ProvinciasBuscarTodos")
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function BuscarPorIdPais(ByVal IdPais As Integer) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("ProvinciasBuscarPorIdPais", IdPais)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function BuscarPorID(ByVal IdProvincia As Integer) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("ProvinciaBuscarPorId", IdProvincia)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function Agregar(ByVal IdProvincia As Integer, ByVal Provincia As String, ByVal Activo As Boolean) As Double
+        Try
+            Return oDatabase.ExecuteScalar("ProvinciaAgregar", IdProvincia, Provincia, Activo)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function Modificar(ByVal IdProvincia As Integer, ByVal IdPais As Integer, ByVal Provincia As String, ByVal Activo As Boolean) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("ProvinciaModificar", IdProvincia, IdPais, Provincia, Activo)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+End Class
