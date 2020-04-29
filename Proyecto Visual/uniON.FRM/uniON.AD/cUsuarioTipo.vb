1e8aa34f81964a3765c1e5d94561ca8d83837912
DanieleNaza <danielenazareno18@gmail.com>
2020-04-29 17:28:17 -0300

    Usuario y permio Tipos

---
+++ Proyecto Visual/uniON.FRM/uniON.AD/cUsuarioTipo.vb
@@ -0,0 +1,52 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports Microsoft.Practices.EnterpriseLibrary.Data
+Public Class cUsuarioTipo
+    Dim oDatabase As Database
+
+
+    Public Sub New()
+        oDatabase = DatabaseFactory.CreateDatabase("Conn")
+    End Sub
+
+    Public Sub New(ByVal str As String)
+    End Sub
+
+    Public Function BuscarPorId(ByVal IdTipoUsuario As Integer) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("UsuarioTiposBuscarPorId", IdTipoUsuario)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function BuscarTodos() As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("UsuariosTiposBuscarTodos")
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function Agregar(ByVal Descripcion As String, ByVal Activo As Boolean) As Double
+        Try
+            Return oDatabase.ExecuteScalar("UsuariosTiposAgregar", Descripcion, Activo)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Function Modificar(ByVal IdTipoUsuario As Integer, ByVal Descripcion As String, ByVal Activo As Boolean) As DataSet
+        Try
+            Return oDatabase.ExecuteDataSet("UsuariosTiposModificar", IdTipoUsuario, Descripcion, Activo)
+        Catch ex As System.Exception
+            Throw ex
+        End Try
+    End Function
+    Public Function Eliminar(ByVal IdTipoUsuario As Integer) As Double
+        Return oDatabase.ExecuteScalar("UsuariosTiposEliminar", IdTipoUsuario)
+    End Function
+
+End Class
---
+++ Proyecto Visual/uniON.FRM/uniON.FRM/frmPremioTipo.vb
@@ -0,0 +1,217 @@
+﻿
+Imports uniON.AD
+
+Public Class frmPremioTipo
+
+#Region "Variables"
+    Private eEstado As EstadodelFormulario
+#End Region
+
+#Region "Enumeraciones"
+    Public Enum EstadodelFormulario
+        eConsulta = 1
+        eAgregar = 2
+        eEditar = 3
+    End Enum
+#End Region
+
+#Region "Propiedades"
+    Public Property Estado() As EstadodelFormulario
+        Get
+            Return eEstado
+        End Get
+        Set(ByVal vNewValue As EstadodelFormulario)
+
+            Select Case vNewValue
+
+                Case EstadodelFormulario.eConsulta
+
+                    Limpiar()
+                    DesHabilitarEdicion()
+                    DesHabilitarComandos()
+                    cmdAgregar.Enabled = True
+                    cmdAceptar.Enabled = False
+                    cmdCancelar.Enabled = False
+                    grlGrilla.Enabled = True
+                    Panel1.BackColor = Color.White
+                    lblAccion.Text = "Consultando"
+                    lblAccion.ForeColor = Color.Black
+
+                Case EstadodelFormulario.eAgregar
+
+                    HabilitarEdicion()
+                    txtIdPremio.Enabled = False
+                    cmdAceptar.Enabled = True
+                    cmdCancelar.Enabled = True
+                    DesHabilitarComandos()
+                    grlGrilla.Enabled = False
+                    Limpiar()
+                    txtDescripcion.Focus()
+                    Panel1.BackColor = Color.MediumAquamarine
+                    lblAccion.Text = "Agregando"
+                    lblAccion.ForeColor = Color.Black
+
+                Case EstadodelFormulario.eEditar
+
+                    HabilitarEdicion()
+                    txtIdPremio.Enabled = False
+                    cmdAceptar.Enabled = True
+                    cmdCancelar.Enabled = True
+                    DesHabilitarComandos()
+                    grlGrilla.Enabled = False
+                    Panel1.BackColor = Color.MediumAquamarine
+                    lblAccion.Text = "Modificando"
+                    lblAccion.ForeColor = Color.Black
+
+            End Select
+            eEstado = vNewValue
+        End Set
+    End Property
+#End Region
+
+#Region "Formulario"
+
+    Private Sub frmPremiosTipo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Estado = EstadodelFormulario.eConsulta
+
+    End Sub
+
+#End Region
+
+#Region "Procedimientos"
+    Private Sub CargarGrilla()
+        BuscarTodos()
+    End Sub
+
+    Private Sub BuscarTodos()
+        Dim oDs As New DataSet
+        Dim oPremioTipo As New cPremiosTipo
+
+        oDs = oPremioTipo.BuscarTodos
+
+        grlGrilla.DataSource = oDs.Tables(0)
+
+        grlGrilla.Columns(0).HeaderText = "Cod."
+        grlGrilla.Columns(0).Width = 50
+
+        oDs = Nothing
+        oPremioTipo = Nothing
+    End Sub
+
+    Private Sub BuscarPorID(ByVal IdTipoPremio As Integer)
+        Dim oDs As New DataSet
+        Dim oPremioTipo As New cPremiosTipo
+
+        oDs = oPremioTipo.BuscarPorId(IdTipoPremio)
+
+        txtIdPremio.Text = oDs.Tables(0).Rows(0).Item("IdTipoPremio")
+        txtDescripcion.Text = oDs.Tables(0).Rows(0).Item("Descripcion")
+        chkActivo.Checked = oDs.Tables(0).Rows(0).Item("Activo")
+
+        oDs = Nothing
+        oPremioTipo = Nothing
+    End Sub
+
+    Private Sub Limpiar()
+        CargarGrilla()
+        txtIdPremio.Text = ""
+        txtDescripcion.Text = ""
+        chkActivo.Checked = True
+    End Sub
+
+    Private Sub HabilitarEdicion()
+        txtIdPremio.Enabled = True
+        txtDescripcion.Enabled = True
+        chkActivo.Enabled = True
+    End Sub
+
+    Private Sub DesHabilitarEdicion()
+        txtIdPremio.Enabled = False
+        txtDescripcion.Enabled = False
+        chkActivo.Enabled = False
+    End Sub
+
+    Private Sub HabilitarComandos()
+        cmdAgregar.Enabled = True
+        cmdModificar.Enabled = True
+    End Sub
+
+    Private Sub DesHabilitarComandos()
+        cmdAgregar.Enabled = False
+        cmdModificar.Enabled = False
+    End Sub
+
+
+#End Region
+
+#Region "Botones de comando"
+    Private Sub cmdAgregar_Click(sender As Object, e As EventArgs)
+        Me.Estado = EstadodelFormulario.eAgregar
+    End Sub
+
+    Private Sub cmdModificar_Click(sender As Object, e As EventArgs) Handles cmdModificar.Click
+        Me.Estado = EstadodelFormulario.eEditar
+    End Sub
+
+    Private Sub cmdAceptar_Click(sender As Object, e As EventArgs) Handles cmdAceptar.Click
+        Try
+            If Validar() = True Then
+
+                Dim oPais As New cPaises
+
+                If Me.Estado = EstadodelFormulario.eEditar Then
+                    oPais.Modificar(txtIdPremio.Text, txtDescripcion.Text, chkActivo.Checked)
+                    MsgBox("Se modificó correctamente el premio con el código nro: " + txtIdPremio.Text, MsgBoxStyle.Information, "Exitos!")
+                End If
+
+                If Me.Estado = EstadodelFormulario.eAgregar Then
+                    Dim resultado As Integer
+                    resultado = oPais.Agregar(txtDescripcion.Text, chkActivo.Checked)
+                    MsgBox("Se agregó correctamente el premio " + txtDescripcion.Text + " con el código nro: " + resultado.ToString, MsgBoxStyle.Information, "Exitos!")
+                End If
+
+                Me.Estado = EstadodelFormulario.eConsulta
+            End If
+        Catch
+            MsgBox("Sucedió un error", MsgBoxStyle.Critical, "Error")
+        End Try
+    End Sub
+
+    Private Sub cmdCancelar_Click(sender As Object, e As EventArgs) Handles cmdCancelar.Click
+        If MsgBox("¿Está seguro que desea cancelar?" & vbCrLf &
+                "Se perderán las últimas modificaciones",
+                vbYesNo, "Confirmación de acción") = MsgBoxResult.Yes Then
+
+            Me.Estado = EstadodelFormulario.eConsulta
+
+        End If
+        Exit Sub
+    End Sub
+
+    Private Sub cmdSalir_Click(sender As Object, e As EventArgs) Handles cmdSalir.Click
+        Close()
+    End Sub
+
+    Private Sub cmdLimpiar_Click(sender As Object, e As EventArgs) Handles cmdLimpiar.Click
+
+        Me.Estado = EstadodelFormulario.eConsulta
+
+    End Sub
+
+
+#End Region
+
+#Region "Funciones"
+    Private Function Validar() As Boolean
+        If txtDescripcion.Text = "" Then
+
+            MsgBox("Complete el nombre del Premio", MsgBoxStyle.Exclamation, "Mensaje")
+
+            Return False
+        End If
+        Return True
+    End Function
+#End Region
+
+
+End Class
